0aecc1f2a932bd140a4b5b68f7d999cf9b266213
ng1n3 <code.with.muyiwa@gmail.com>
2024-02-24 11:56:07 +0100

    conditional output and reset

--- src/components/TipCalculator.tsx
+++ src/components/TipCalculator.tsx
@@ -4,30 +4,37 @@ import Output from "./Output";
 import SelectPercentage from "./SelectPercentage";
 import Reset from "./Reset";
 
-
 const TipCalculator: React.FC = () => {
   const [bill, setBill] = useState<number>(0);
   const [firstPercent, setFirstPercent] = useState<number>(0);
   const [secondPercent, setSecondPercent] = useState<number>(0);
 
   function handleReset() {
-    setBill(0)
-    setFirstPercent(0)
-    setSecondPercent(0)
+    setBill(0);
+    setFirstPercent(0);
+    setSecondPercent(0);
   }
 
   const tip = bill * ((firstPercent + secondPercent) / 2 / 100);
   return (
     <div>
-      <BillInput bill={bill} onSetBill={setBill}/>
-      <SelectPercentage percentage={firstPercent} onSelect={setFirstPercent}>How did you like our service</SelectPercentage>
-  
-      <SelectPercentage percentage={secondPercent} onSelect={setSecondPercent}>How did you like our service</SelectPercentage>
+      <BillInput bill={bill} onSetBill={setBill} />
+      <SelectPercentage percentage={firstPercent} onSelect={setFirstPercent}>
+        How did you like our service
+      </SelectPercentage>
 
-      <Output  bill={bill} tip={tip}/>
-      <Reset onReset={handleReset}/>
+      <SelectPercentage percentage={secondPercent} onSelect={setSecondPercent}>
+        How did you like our service
+      </SelectPercentage>
+
+      {bill > 0 && (
+        <>
+          <Output bill={bill} tip={tip} />
+          <Reset onReset={handleReset} />
+        </>
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default TipCalculator;
